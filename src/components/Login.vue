bd3e11fd7b8dc1972dfdffc97060e608993e137f
王嘉绩 <5111535+gggakki@user.noreply.gitee.com>
2019-12-16 01:14:11 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="login_container">
+1111
+</div>
+
+</template>
+
+<script>
+    export default{
+        name:'Login'
+    }
+</script>
+
+<style lang="less"  scoped>
+.login_container{
+    width: 100%;
+    height: 100%;
+    background-color: #2b4b6b;
+}
+
+</style>
